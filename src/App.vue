ae84fd769a852c92c5709150f674c9856b04ed9d
ibuki2003 <main@fuwa.dev>
2020-05-13 10:46:21 +0900

    feat: create template

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,14 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <header>
+      <b-navbar type="dark" variant="dark">
+        <b-navbar-brand :to="{ name: 'Home' }"
+          >Google Classroom Tools</b-navbar-brand
+        >
+      </b-navbar>
+    </header>
+    <main class="container my-4 py-4 bg-white shadow-sm rounded">
+      <router-view></router-view>
+    </main>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
