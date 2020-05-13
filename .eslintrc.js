1bef44ff2b54002e5947617dcd02f87d62bfe77d
ibuki2003 <main@fuwa.dev>
2020-05-13 10:23:04 +0900

    build: configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,43 @@
+const rulesDirPlugin = require("eslint-plugin-rulesdir");
+rulesDirPlugin.RULES_DIR = "rules";
+
 module.exports = {
   root: true,
   env: {
     node: true
   },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
-  ],
+  extends: ["eslint:recommended", "@vue/prettier"],
   parserOptions: {
     ecmaVersion: 2020
   },
+
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "warn",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "warn"
+  },
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+      ],
+      env: {
+        mocha: true
+      }
+    },
+    {
+      files: ["**/*.ts", "**/*.vue"],
+      plugins: ["rulesdir"],
+      extends: [
+        "@vue/typescript/recommended",
+        "@vue/prettier/@typescript-eslint",
+        "plugin:vue/essential"
+      ],
+      rules: {
+        "@typescript-eslint/camelcase": "off",
+        "@typescript-eslint/no-explicit-any": "error",
+        "rulesdir/vue-template-simple-expr": "error"
+      }
+    }
+  ]
 };
